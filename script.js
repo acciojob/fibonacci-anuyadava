89071f8b731fddff1795062271ff409e657c0d0d
anuyadava <yadavannu9235@gmail.com>
2024-08-12 19:11:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function fibonacci(num) {
-// your code here
-}
+def fibonacci(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    
+    # Initialize the first two Fibonacci numbers
+    a, b = 0, 1
+    
+    # Compute Fibonacci number from 2 up to num
+    for _ in range(2, num + 1):
+        a, b = b, a + b
+    
+    return b
 
-module.exports = fibonacci;
